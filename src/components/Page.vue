5c796447d04259329dd34010fb0e21da16e511f0
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-05-19 13:21:59 +0300

    Created Page component

---
+++ src/components/Page.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="page-container">
+        <p>PAGEEE</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Page',
+  components:{
+  },
+  props: {
+  }
+}
+</script>
+
+<style>
+.page-container{
+
+}
+</style>
