3c07d0a595439c16c07ec0b0b9448a8404e5c9bc
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-05-19 12:22:33 +0300

    Created CVPage.vue component and rendered it to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,18 @@
 <template>
-  <HelloWorld/>
+  <CVPage/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CVPage from './components/CVPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CVPage
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/CVPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="CVPage-container">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CVPage',
+  components: '',
+  props: {
+  }
+}
+</script>
+
+
+<style scoped>
+.CVPage-container{
+
+}
+</style>
