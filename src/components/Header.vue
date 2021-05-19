00af0b393d3001cc348a78b4b3b31aa1b380af9c
GiuliaRadu <radugiulia17@gmail.com>
2021-05-19 13:21:59 +0300

    Created Header component, updated package-lock after npm install

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header-container"></div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {},
+};
+</script>
+
+<style>
+.header-container {
+}
+</style>
