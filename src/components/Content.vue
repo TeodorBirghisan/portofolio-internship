7aa16a4190ca29a2ba15c870e3d90aa3997f5f7d
AzureAD\Alexandra-GeorgianaP <alexandra.puchianu@principal33.com>
2021-05-19 13:43:25 +0300

    Created Content component and rendered it in Page component.

---
+++ src/components/Content.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="content-container">
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Content',
+  components: {}
+}
+</script>
+
+<style scoped>
+
+</style>
