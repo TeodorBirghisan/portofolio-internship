977e6470aeaeed1e2a703b82a539d18d3ec74b8a
AzureAD\Alexandra-GeorgianaP <alexandra.puchianu@principal33.com>
2021-05-20 12:22:15 +0300

    Added avatar image in Sidebar component.

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,33 +1,38 @@
 <template>
-    <div class="sidebar-container">
-      <Contact/>
-      <Hobbies/>
-      <Skills/>
-    </div>
+  <div class="sidebar-container">
+    <img id="avatar" src="../../public/avatar.png" />
+    <Contact />
+    <Hobbies />
+    <Skills />
+  </div>
 </template>
 
 <script>
-import Contact from './Contact'
-import Hobbies from './Hobbies'
-import Skills from './Skills'
+import Contact from "./Contact";
+import Hobbies from "./Hobbies";
+import Skills from "./Skills";
 
 export default {
-  name: 'Sidebar',
+  name: "Sidebar",
   components: {
     Contact,
     Hobbies,
-    Skills
+    Skills,
   },
-  props: {
-  }
-}
+  props: {},
+};
 </script>
 
-
 <style scoped>
-.sidebar-container{
+.sidebar-container {
   background: red;
   width: 25%;
   height: 100%;
+}
+#avatar {
+  width: 100%;
+  padding-top: 1rem;
+  padding-left: 4rem;
+  padding-right: 4rem;
 }
 </style>
