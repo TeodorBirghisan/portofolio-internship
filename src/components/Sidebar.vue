e62318fb0a89cc96c2646e2898e72e4388a7936b
AzureAD\Alexandra-GeorgianaP <alexandra.puchianu@principal33.com>
2021-05-20 14:30:51 +0300

    Maked Sidebar content section scrollable.

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="sidebar-container">
-    <img id="avatar" src="../../public/avatar.png" />
-    <Contact />
-    <Skills />
-    <Hobbies />
+    <div class="avatar-container">
+      <img id="avatar" src="../../public/avatar.png" />
+    </div>
+
+    <div class="sidebar-content-scrollable">
+      <Contact />
+      <Skills />
+      <Hobbies />
+    </div>
   </div>
 </template>
 
@@ -28,9 +33,41 @@ export default {
   background: red;
   width: 25%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
 }
-#avatar {
+
+.sidebar-content-scrollable {
   width: 100%;
+  height: 70%;
+  overflow: auto;
+}
+
+.sidebar-content-scrollable::-webkit-scrollbar {
+  width: 10px;
+}
+
+.sidebar-content-scrollable::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+.sidebar-content-scrollable::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+.sidebar-content-scrollable::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+
+.avatar-container {
+  width: 100%;
+  height: 30%;
+  display: flex;
+  justify-content: center;
+}
+
+#avatar {
+  height: 100%;
   padding-top: 1rem;
   padding-left: 4rem;
   padding-right: 4rem;
