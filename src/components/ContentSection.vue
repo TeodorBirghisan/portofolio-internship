0a9656342c427e686ebdd42074afe5fa6b57f1b4
AzureAD\Alexandra-GeorgianaP <alexandra.puchianu@principal33.com>
2021-05-20 12:08:59 +0300

    Added style for ContentSection component.

--- src/components/ContentSection.vue
+++ src/components/ContentSection.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="content-section-container">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, corrupti.
+      Nihil, beatae, molestiae quasi animi exercitationem dolorem temporibus quo
+      praesentium fuga vero modi! Iste sequi doloremque tempore! Neque, modi
+      quae.
+    </p>
   </div>
 </template>
 
@@ -11,5 +17,11 @@ export default {
 
 <style>
 .content-section-container {
+  padding-left: 1rem;
+  padding-top: 1rem;
+  width: 100%;
+  height: auto;
+  background-color:blueviolet;
+  padding-right: 3rem;
 }
 </style>
