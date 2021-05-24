4e732e28689a454f8081a9a2cf432fbab3941401
GiuliaRadu <radugiulia17@gmail.com>
2021-05-24 12:40:03 +0300

    Added Header stylization

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="header-container"></div>
+  <div class="header-container">
+    <h1 class="header-title">{{title}}</h1>
+    <hr size="3" width="50%" color="grey">
+    <h4 class="header-subtitle text-muted">{{occupation}}</h4>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Header",
-  props: {},
+  props: {
+    title: String,
+    occupation: String
+  },
 };
 </script>
 
@@ -13,6 +20,33 @@ export default {
 .header-container {
   width: 100%;
   height: 20%;
-  background-color: antiquewhite;
+  background-color: #F4F4F4;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.header-container hr{
+    margin :0;
+    color: rgb(44, 44, 44);
+    background-color: currentColor;
+    border: 0;
+    opacity: .25;
+}
+.header-title {
+  color:rgb(53, 53, 53);
+  font-family: serif;
+  font-variant-caps:all-small-caps;
+}
+
+/* .header-title h1{
+  font-size: 32px;
+} */
+
+.header-subtitle {
+  padding-top: 1px;
+  font-family: serif;
+  margin-top: 5px;
 }
 </style>
