3b1702eb0503168147c42c98a914910db3d74ec7
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-05-24 21:31:46 +0300

    Added input fields and button

---
+++ src/components/auth/Login.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="Login-container">
+<div class="form-outline">
+  <input type="email" id="typeEmail" class="form-control" />
+  <label class="form-label" for="typeEmail">Email input</label>
+</div>
+<div class="form-outline">
+  <input type="password" id="typePassword" class="form-control" />
+  <label class="form-label" for="typePassword">Password input</label>
+</div>
+<button type="button" class="btn btn-primary btn-rounded">Primary</button>
+</div>
+</template>
+
+<script>
+//import { Form, Field, ErrorMessage } from "vee-validate";
+//import * as yup from "yup";
+export default {
+name: 'Login',
+components: {},
+}
+</script>
+
+<style scoped>
+</style>
