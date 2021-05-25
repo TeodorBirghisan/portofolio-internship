b552be140984d4daef575456f8a87fc113b064c6
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-05-25 12:42:58 +0300

    created Navigation Bar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,58 @@
 <template>
-  <Login/>
+<div id="app">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <a href="/" class="navbar-brand">CV PAAGE</a>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link v-if="currentUser" to="/user/1" class="nav-link">User</router-link>
+        </li>
+      </div>
+
+      <div v-if="!currentUser" class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link to="/register" class="nav-link">
+             Sign Up
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/login" class="nav-link">
+            Login
+          </router-link>
+        </li>
+      </div>
+
+      <div v-if="currentUser" class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" @click.prevent="logOut">
+            LogOut
+          </a>
+        </li>
+      </div>
+    </nav>
+    <div class="container">
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <script>
 //import CVPage from './components/CVPage.vue'
-import Login from './components/auth/Login';
+//import Login from './components/auth/Login';
 export default {
   name: 'App',
+  computed:{
+    currentUser(){
+      return this.$store.state.auth.user;
+    }
+  },
   components: {
-    Login
+  },
+  methods: {
+    logOut(){
+      console.log('HEEIiio')
+      this.$store.dispatch('auth/logout');
+      this.$router.push('/login');
+    }
   }
 }
 </script>
